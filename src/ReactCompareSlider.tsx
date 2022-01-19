61af2173d2d67abec37f606902feca62afb0d55f
brandon park <brandonpark206@gmail.com>
2022-01-18 17:31:17 -0800

    fix-null ref issue

--- src/ReactCompareSlider.tsx
+++ src/ReactCompareSlider.tsx
@@ -168,12 +168,12 @@ export const ReactCompareSlider: React.FC<
         // Use height *or* width based on orientation.
         (_portrait ? adjustedHeight : adjustedWidth) - _boundsPadding
       );
-      if (clipContainerRef) {
+      if (clipContainerRef && clipContainerRef.current) {
         (clipContainerRef.current as HTMLElement).style.clip = _portrait
           ? `rect(auto,auto,${clampedPx}px,auto)`
           : `rect(auto,${clampedPx}px,auto,auto)`;
       }
-      if (handleContainerRef) {
+      if (handleContainerRef && handleContainerRef.current) {
         (handleContainerRef.current as HTMLElement).style.transform = _portrait
           ? `translate3d(0,${clampedPx}px,0)`
           : `translate3d(${clampedPx}px,0,0)`;
